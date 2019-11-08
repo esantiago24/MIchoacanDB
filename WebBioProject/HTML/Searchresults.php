44df891ec88fb9f6fa0806bc8844e554112f01df
Arturolcgej <agarcia@lcgej.unam.mx>
2019-11-08 14:11:21 -0600

    Actualizacion de Results Page

--- WebBioProject/HTML/Searchresults.php
+++ WebBioProject/HTML/Searchresults.php
@@ -1,54 +1,80 @@
 <html>
+
 	<head>
-                <title>Search Results</title>
-                <link rel="icon" href="./Images/LF.ico"> <!--Webpage Icon -->
-                <link href="../CSS/Searchresults.css" rel="stylesheet" type="text/css" />
+
+		<meta charset="utf-8">
+		<title> E. Coli Gene Database </title>
+		<link rel="icon" href="../Ima/LF.ico"> <!--Webpage Icon -->
+		<link href="../CSS/Mainbodyres.css" rel="stylesheet" type="text/css" /> <!--Link to css file-->
+		<link href="../CSS/Menu.css" rel="stylesheet" type="text/css" />
 		<link href="../CSS/Searchresultsbar.css" rel="stylesheet" type="text/css" />
 		<script src="https://kit.fontawesome.com/43eb676244.js" crossorigin="anonymous"></script>
+
+		<!--The following lines put all the Webpage margins in 0-->
+		<style type="text/css">
+		body {
+			margin-left: 0px;
+			margin-top: 0px;
+			margin-right: 0px;
+			margin-bottom: 0px;
+		}
+		</style>
 
 	</head>
 
 	<body>
 		<div id="Container">
-			<div id="Header">
-				<a href="./InterfazWeb.html">
-				<img src="../Ima/LogHeader.png" alt="" />
-				</a>
-			<div class="Text">
-				<h1>E. Coli Gene Database</h1>
-                        </div>
-			</div>
-		</div>
-
-		<div id="MainBody"> <!--MainBody contains the search bar-->
-
-			<form id="form2" name="form2" method="get" action="Searchresults.php">
-
-				<div class="search-box">
-					<input class="search-txt" type="text" name="search" placeholder="Type to search">
-
-					<div class="Checkboxes"> <!--Search Parameters-->
-						<input type="checkbox" name="gene" id="gene" checked="checked"> Gene
-						<input type="checkbox" name="promoter" id="promoter" checked="checked"> Promoter
-						<input type="checkbox" name="tu" id="tu" checked="checked"> Transcriptional Unit
+			<div id="Content-Wrap">
+				<div id="Header">
+					<img src="../Ima/LogHeader.png" alt="" />
+					<div class="Text">
+						<h1>E. Coli Gene Database</h1>
 					</div>
-
-					<button type="submit" name="submit" class="search-btn">
-					<i class="fas fa-search"></i>
-					</button>
-
 				</div>
 
+				<!--The following lines contain the Menu set up-->
+				<div id="Menu">
+					<ul class="EMenu">
+						<li><a href="#" class="has-sub">Home</a></li>
+						<li><a href="#" class="has-sub">Download</a>
+							<ul>
+							<li><a href="#">Dataset</a></li>
+							<li><a href="#">Dump</a></li>
+							</ul>
+						</li>
+						<li><a href="#" class="has-sub">Users</a>
+							<ul>
+							<li><a href="./Login.html">Login</a></li>
+							<li><a href="./Register.html">Register</a></li>
+							</ul>
+						</li>
+						<li><a href="#">About us</a></li>
+						<li><a href="#">Contact</a></li>
+					</ul>
+				</div> <!--End Menu-->
 
-	<?php
-			error_reporting(E_ALL);
- 			ini_set('display_errors', '1');
-			$gene_search = escapeshellcmd($_GET["search"]);
-			echo $gene_search;
+				<div id="SideBar"></div>
 
-			// MySQL connection
-			$mysqli = new mysqli("132.248.248.120:3306", "regulonuser", "ReguL1igh22#", "regulondb");
-		?>
+				<div id="MainBody"> <!--MainBody contains the search bar-->
+
+					<form id="form1" name="form1" method="get" action="Searchresults.php">
+
+					<div class="search-box">
+						<input class="search-txt" type="text" name="search" placeholder="Type to search">
+						<div class="Checkboxes"> <!--Search Parameters-->
+							<input type="checkbox" name="gene" id="gene" checked="checked"> Gene </br>
+							<br><input type="checkbox" name="promoter" id="promoter" checked="checked"> Promoter</br>
+							<br><input type="checkbox" name="tu" id="tu" checked="checked"> Transcriptional Unit</br>
+						</div>
+						<button type="submit" name="submit" class="search-btn">
+						<i class="fas fa-search"></i>
+						</button>
+					</div>
+					</form><!--End Form-->
+
+				</div> <!--End MainBody-->
+			</div> <!--End Content Wrap-->
+		</div> <!--End Container-->
 	</body>
 
 </html>
