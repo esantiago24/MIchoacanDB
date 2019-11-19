1704de043d4afb8330d87a22a023e9e0d971162a
Arturolcgej <agarcia@lcgej.unam.mx>
2019-11-19 17:10:16 -0600

    Agregue cartpeta Display_dataset.php

---
+++ WebBioProject/HTML/Display_dataset.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<title>Display Dataset</title>
+	</head>
+
+	<body>
+
+<iframe src="Dataset.txt" style="height:300px;width:400px;"></iframe> <br><br><br>
+
+<a href="./Dataset.txt" download= "ecolidb_dataset.tsv" >Download dataset </a>
+
+	</body>
+</html>
