80e548515af544ef1817f3bd9033575f3f879ba9
esantiago24 <esantiago@lcgej.unam.mx>
2019-11-19 19:48:42 -0600

    Cambié las rutas relativas.

--- WebBioProject/HTML/Validate_mail.php
+++ WebBioProject/HTML/Validate_mail.php
@@ -21,23 +21,26 @@ $mysqli = new mysqli("localhost:3306", "esantiago", "M1n0mbr3","Erick");
 //CREATE TABLE ecolidb_users(Name VARCHAR(100), Last_Name VARCHAR(100), Mail VARCHAR(100) PRIMARY KEY, Institution VARCHAR(100), uso ENUM("academic","private") ) ;
 
 $result = $mysqli->query("SELECT Mail FROM ecolidb_users WHERE Mail='".$mail."' ");
+error_log("______ROWS:_______".$result->num_rows, 0);
   if($result->num_rows < 1){
     error_log("______ENTRA A RESULT_______", 0);
     $query = "INSERT INTO ecolidb_users (Name,Last_Name, Mail, Institution, Uso) VALUES ('$name', '$last_name', '$mail','$inst','$uso')";
     $success= $mysqli->query($query);
+    error_log("______SUCCESS:_______".$success, 0);
     if($success){
       error_log("______ENTRA A SUCCESS_______", 0);
       $mysqli->close();
-      header("Location: ./Successful.html");
+      header("Location: ./Sucregister.html");
+
     } else {
       error_log("______ELSE SUCCESS_______", 0);
       $mysqli->close();
-       header("Location: ./Reg_failure.html");
+       header("Location: ./ErrorReg.html");
     }
   }else{
     error_log("______ELSE RESULT_______", 0);
     $mysqli->close();
-    header("Location: ./Existing_user.html");
+    header("Location: ./Existuser.html");
   }
 
   ?>
